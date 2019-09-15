ffd879c92fe7f0f9197df5246b65a31ae342628a
Max Burke <maxburke@gmail.com>
2019-09-14 19:28:36 -0700

    changes

--- fn.lisp
+++ fn.lisp
@@ -8,3 +8,11 @@
   (let ((db (if (assoc name *fn-db*) (remove name *fn-db* :key #'car) *fn-db*))
         (setf *fn-db* (acons name compile-body) db)))
   )
+
+(defun link ())
+
+(defun emit-lx (name object-code)
+  (with-open-file (out name :element-type '(unsigned-byte 8) :direction output)
+    (write-byte (char-code #\M) out)
+    (write-byte (char-code #\Z) out)))
+
